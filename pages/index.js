19957e9b731a2afd192b8056981d18bb2f631b41
YahyaOmari <yahya.omari@outlook.com>
2021-07-05 13:47:53 +0300

    added styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Head1 from '../component/Head'
 import Header from '../component/Header'
 import Footer from '../component/Footer'
@@ -8,7 +7,8 @@ import Main from '../component/Main'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    
+    <div className="flex flex-col items-center justify-center w-full h-full overflow-x-hidden text-2xl bg-green-100 ">
       <Head1 title='Cookie Stand Admin' >
 
       </Head1>
