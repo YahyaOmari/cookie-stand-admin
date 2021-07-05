95caef75be2320300f4045ccb534f1598ae385cc
YahyaOmari <yahya.omari@outlook.com>
2021-07-05 14:04:49 +0300

    testing styles

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Footer from '../component/Footer'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center w-full h-full overflow-x-hidden text-2xl bg-green-100 ">
       <Head>
         <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
