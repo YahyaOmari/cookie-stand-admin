b9d296b454ca26e9791081c311bdaed9d687c819
YahyaOmari <yahya.omari@outlook.com>
2021-07-05 14:14:40 +0300

    testing styles

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
-
-import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Head1 from '../component/Head'
 import Header from '../component/Header'
-import Main from '../component/Main'
 import Footer from '../component/Footer'
+import Main from '../component/Main'
+
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full overflow-x-hidden text-2xl bg-green-100 ">
-      <Head>
-        <title>Cookie Stand Admin</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Head1 title='Cookie Stand Admin' >
 
-    </div>)}
+      </Head1>
+
+      <Header >
+
+      </Header>
+
+      <Main >
+
+      </Main>
+      
+      <Footer >
+        
+      </Footer>
+    </div>
+  )
+}
